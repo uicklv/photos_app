d1986362a60f7900763c751b68bfdf499cd6ff4b
Topor Nickolay <uicklv@gmail.com>
2022-06-08 21:39:56 +0300

    tested components

---
+++ app/View/Components/Alert.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\View\Components;
+
+use Illuminate\View\Component;
+
+class Alert extends Component
+{
+    public $type;
+
+    public $dismissible;
+
+    protected $types = [
+        'success',
+        'danger',
+        'warning',
+        'info',
+    ];
+
+    /**
+     * Create a new component instance.
+     *
+     * @return void
+     */
+    public function __construct($type = "info", $dismissible = false)
+    {
+        $this->type = $type;
+        $this->dismissible = $dismissible;
+    }
+
+    public function validType()
+    {
+        return in_array($this->type, $this->types) ? $this->type : 'info';
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     *
+     * @return \Illuminate\Contracts\View\View|\Closure|string
+     */
+    public function render()
+    {
+        return view('components.alert');
+    }
+}
---
+++ resources/views/components/alert.blade.php
@@ -0,0 +1,10 @@
+<div {{ $attributes->class(['alert-dismissible fade show' => $dismissible])->merge(['class' => "alert alert-{$validType}", 'role' => $attributes->prepends('alert')]) }}>
+    @isset($title)
+        <h4 class="alert-heading">{{ $title }}</h4>
+    @endisset
+    {{ $slot }}
+    @if($dismissible)
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    @endif
+</div>
+
