2d3051e5d758dfcba0b9c73886b5a6029de674d4
Topor Nickolay <uicklv@gmail.com>
2022-06-10 23:28:22 +0300

    templates

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Str;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
@@ -41,4 +42,16 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function images()
+    {
+        return $this->hasMany(Image::class);
+    }
+
+    public function getImagesCount()
+    {
+        $imagesCount = $this->images()->published()->count();
+
+        return $imagesCount . " " . Str::plural('image', $imagesCount);
+    }
 }
--- resources/views/image/edit.blade.php
+++ resources/views/image/edit.blade.php
@@ -1,14 +1,34 @@
-<h1>Edit image</h1>
-<x-form action="{{ route('images.update', $image->id) }}" method="PUT">
-    <div>
-       <img src="{{ $image->fileUrl() }}" alt="{{ $image->title }}" width="400">
+<x-layout title="Update Image">
+
+    <div class="container-fluid mt-4">
+        <div class="row">
+            <div class="col-md-6 offset-md-3">
+                <div class="card">
+                    <div class="card-header">Update Image</div>
+
+                    <div class="card-body">
+
+                        <x-form action="{{ route('images.update', $image->id) }}" method="PUT">
+                            <div class="mb-4">
+                                <img src="{{ $image->fileUrl() }}" alt="{{ $image->title }}" class="img-fluid">
+                            </div>
+                            <div class="mb-4">
+                                <lable class="form-label" for="title">Title</lable>
+                                <input class="form-control @error('title') is-invalid @enderror" type="text" name="title" id="title" value="{{ old('title', $image->title) }}">
+                                @error('title')
+                                <div class="invalid-feedback">{{ $message }}</div>
+                                @enderror
+                            </div>
+                            <div class="mb-3">
+                                <button type="submit" class="btn btn-primary">Update</button>
+                                <a href="{{ route('images.index') }}" class="btn btn-outline-secondary">Cancel</a>
+                            </div>
+                        </x-form>
+                    </div>
+                </div>
+
+            </div>
+        </div>
     </div>
-    <div>
-        <lable for="title">Title</lable>
-        <input type="text" name="title" id="title" value="{{ old('title', $image->title) }}">
-        @error('title')
-            <div>{{ $message }}</div>
-        @enderror
-    </div>
-    <button type="submit">Update</button>
-</x-form>
+</x-layout>
+
